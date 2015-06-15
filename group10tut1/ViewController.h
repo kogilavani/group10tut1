a971d3594af8b14f9788ab4e61c254e2977d9428
Janga <lj8rd@kc-sce463lab-12.kc.umkc.edu>
2015-06-15 15:12:48 -0500

    Initial Commit

---
+++ group10tut1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  group10tut1
+//
+//  Created by Janga, Likitha (UMKC-Student) on 6/15/15.
+//  Copyright (c) 2015 Janga, Likitha (UMKC-Student). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ group10tut1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  group10tut1
+//
+//  Created by Janga, Likitha (UMKC-Student) on 6/15/15.
+//  Copyright (c) 2015 Janga, Likitha (UMKC-Student). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
